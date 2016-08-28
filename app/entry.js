01b5a53265c5a092a97fd94fc7d5f27bf2ad28c0
ron oh <ronoh2001@gmail.com>
2016-08-28 15:52:22 -0700

    initial commit

---
+++ app/entry.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// webpack assets
+require('!!file?name=[name].[ext]!./html/index.html');
+require('./scss/base.scss');
+
+// npm modules
+const angular = require('angular');
+
+// angular modules
+angular.module('demoApp', []);
+
+// angular services
+
+// angular components
+require('./component/main');
